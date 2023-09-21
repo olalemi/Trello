b39cb37e108b99af258d1d32b2de0ba61c9fed74
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2023-09-21 11:57:46 +0100

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Fragment } from "react";
 import Board from "./component/Board";
-import { DndProvider } from "react-dnd"; // Import DndProvider from 'react-dnd' instead of 'react-dnd-html5-backend'
-import { HTML5Backend } from "react-dnd-html5-backend"; // Import HTML5Backend from 'react-dnd-html5-backend'
-
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
     <Fragment>
- 
-      {/* Wrap the board with the DndProvider */}
       <DndProvider backend={HTML5Backend}>
-      <Board />
+        <Board />
       </DndProvider>
-
     </Fragment>
   );
 }
